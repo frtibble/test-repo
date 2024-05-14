402971f6ab7b2b375ad8bf2912f3d3863afb4476
Frances Tibble <frtibble@microsoft.com>
2024-05-14 16:09:48 +0100

    ci: analyse commits

--- release.config.js
+++ release.config.js
@@ -3,21 +3,6 @@ module.exports = {
     'main'
   ],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/changelog',
-      {
-        changelogFile: 'CHANGELOG.md'
-      }
-    ],
-    '@semantic-release/github',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md'],
-        message: 'chore(release): set release to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ]
+    '@semantic-release/commit-analyzer'
   ]
 }
